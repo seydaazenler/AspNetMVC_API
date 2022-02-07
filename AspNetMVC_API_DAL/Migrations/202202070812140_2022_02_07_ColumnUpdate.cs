df44309d90903f928f7b115503b0b2b13b690d7a
Şeyda Zenler <seydaazenler@gmail.com>
2022-02-07 13:07:37 +0300

    Students database oluşturuldu.WebAPI ye giriş yapıldı.

---
+++ AspNetMVC_API_DAL/Migrations/202202070812140_2022_02_07_ColumnUpdate.cs
@@ -0,0 +1,16 @@
+﻿namespace AspNetMVC_API_DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _2022_02_07_ColumnUpdate : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ AspNetMVC_API_DAL/Migrations/202202070836225_UpdateDatabase.cs
@@ -0,0 +1,20 @@
+﻿namespace AspNetMVC_API_DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateDatabase : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Students", "RegisterDate", c => c.DateTime(nullable: false));
+            DropColumn("dbo.Students", "RegisterDate");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Students", "RegisterDate", c => c.DateTime(nullable: false));
+            DropColumn("dbo.Students", "RegisterDate");
+        }
+    }
+}
