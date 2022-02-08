bb4b1421b2e22458dffa3520d0b41cf7250055dc
Şeyda Zenler <seydaazenler@gmail.com>
2022-02-08 11:58:19 +0300

    WebService ile temel CRUD işlemleri için metotlar yazıldı. SoapUI aracılığıyla test edildi.

--- AspNetMVC_API/WebServiceStudent.asmx.cs
+++ AspNetMVC_API/WebServiceStudent.asmx.cs
@@ -1,5 +1,6 @@
 ﻿using AspNetMVC_API_BLL.Repository;
 using AspNetMVC_API_Entity.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -61,8 +62,10 @@ namespace AspNetMVC_API
                 if (insertResult>0)
                 {
                     //birinci yöntem
-                    return "Kayıt başarılı olarak eklendi. id=" +newStudent.Id;
+                    //return "Kayıt başarılı olarak eklendi. id=" +newStudent.Id;
                     //ikinci yöntem
+                    string jsonString = JsonConvert.SerializeObject(newStudent);
+                    return jsonString;
                 }
                 else
                 {
@@ -72,6 +75,86 @@ namespace AspNetMVC_API
             catch (Exception ex)
             {
                 //string ile dönüş olduğu için ex.message gödneririz
+                return ex.Message;
+            }
+        }
+
+        [WebMethod]
+        public string Delete(int id)
+        {
+            try
+            {
+                if (id>0)
+                {
+                    Student student = myStudentRepo.GetById(id);
+                    if (student == null)
+                    {
+                        throw new Exception("Öğrenci bulunamadığı için silme işlemi başarısız!");
+                    }
+                    int deleteResult = myStudentRepo.Delete(student);
+                    if (deleteResult>0)
+                    {
+                        return "Kayıt başarıyla silindi!";
+                    }
+                    else
+                    {
+                        throw new Exception("Beklenmedik bir hata oluştuğu için kayıt silinemedi!");
+                     }
+                }
+                else
+                {
+                    throw new Exception("Gönderilen id değeri sıfırdan büyük olmalıdır!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                return ex.Message;
+            }
+        }
+
+        [WebMethod]
+        public string Update(int currentid, string newname, string newsurname)
+        {
+            try
+            {
+                if (currentid<=0)
+                {
+                    throw new Exception("Gönderilen id değeri sıfırdan büyük olmalıdır!");
+                }
+                if (string.IsNullOrEmpty(newname) && string.IsNullOrEmpty(newsurname))
+                {
+                    throw new Exception("(newname-newsurname) alanları boş boırakılamaz!");
+                }
+                Student currentStudent = myStudentRepo.GetById(currentid);
+                if (currentStudent==null)
+                {
+                    throw new Exception("Öğrenci bulunamadığı için güncelleme işlemi başarısızdır!");
+                }
+                //eğer newname parametresi dolu ise isim güncellenecek
+                if (!string.IsNullOrEmpty(newname))
+                {
+                    currentStudent.Name = newname;
+                }
+
+                //eğer newsurname parametresi dolu ise soyisim güncellenecek
+                if (!string.IsNullOrEmpty(newsurname))
+                {
+                    currentStudent.Surname = newsurname;
+                }
+                int updateResult = myStudentRepo.Update();
+                if (updateResult>0)
+                {
+                    return "Güncelleme işlemi başarıyla gerçekleşti";
+                }
+                else
+                {
+                    throw new Exception("Beklenmedik bir hata nedeniyle kayıt güncellenemedi");
+                }
+            }
+            catch (Exception ex)
+            {
                 return ex.Message;
             }
         }
